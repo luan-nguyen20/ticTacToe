812a4676467cdebb624464352cde3e644e18f09a
Luan <nguyenbaoluan1989@gmail.com>
2020-03-05 17:25:17 -0600

    finished AI

--- script.js
+++ script.js
@@ -1,160 +1,150 @@
-// const cells = Array.from(document.querySelectorAll('.cell'));
-// const message = document.querySelector('.message');
-// const restartBtn = document.querySelector('.restartBtn');
-
-// cells.forEach(cell => {
-//     cell.addEventListener('click',e => {
-//         play(e);
-//     });
-// });
-
-// restartBtn.addEventListener('click', restart);
-
-// let val = ' ';
-// function play(e){
-//     if(e.target.textContent === ' '){ // if cell is blank
-//         val === 'X' ? val = 'O' : val = 'X'; // alternate X and O
-//         e.target.textContent = val;
-//         checkVictory();
-//     }
-// } 
-
-// function checkVictory(){
-//     const winCombo = [[0,1,2],[3,4,5],[6,7,8], // 3 rows
-//                     [0,3,6],[1,4,7],[2,5,8], // 3 cols
-//                     [0,4,8],[2,4,6]]; // 2 diagonals
-
-//     winCombo.forEach(combo => {
-//         if(cells[combo[0]].textContent===cells[combo[1]].textContent 
-//             && cells[combo[1]].textContent===cells[combo[2]].textContent 
-//             && cells[combo[0]].textContent !== " "){
-//             message.textContent = "Player " + val + " wins!";
-//             cells.forEach(cell => {
-//                 cell.classList.add("disabled");
-//             });
-//         }
-//     })
-// }
-
-// function restart(){
-//     val = ' ';
-//     message.textContent = ' ';
-//     cells.forEach(cell => {
-//         cell.textContent = ' ';
-//         cell.classList.remove("disabled");
-//     });
-// }
-
 let originalBoard;
-const humanPlayer = "O";
-const comPlayer = "X";
-let winnerFound = false;
+const humanPlayer = 'O';
+const comPlayer = 'X';
+const winCombos = [
+	[0, 1, 2], [3, 4, 5], [6, 7, 8], //3 rows
+	[0, 3, 6], [1, 4, 7], [2, 5, 8], //3 columns
+	[0, 4, 8], [6, 4, 2] //2 diagonals
+];
 
-const winCombos = [[0,1,2],[3,4,5],[6,7,8], // 3 rows
-                    [0,3,6],[1,4,7],[2,5,8], // 3 cols
-                    [0,4,8],[2,4,6]]; // 2 diagonals
-
+const cells = document.querySelectorAll('.cell');
 const message = document.querySelector('.message');
-const cells = Array.from(document.querySelectorAll('.cell'));
 const restartBtn = document.querySelector('.restartBtn');
 restartBtn.addEventListener('click', startGame);
 
-//helper funcs
-//*****************************************************************
-function disableCells(){
-    cells.forEach(cell => {
-        cell.classList.add("disabled");
-    });
-}
-
-function enableCells(){
-    cells.forEach(cell => {
-        cell.classList.remove("disabled");
-    });
-}
-//*****************************************************************
-
 startGame();
 
-function startGame(){
-    winnerFound = false;
-    message.textContent = ' ';
-    originalBoard = Array.from(Array(9).keys()); //create array with 9 elems, values from 0-8.
-    enableCells();
-    cells.forEach(cell => {
-        cell.textContent = ' ';
-        cell.style.removeProperty('background-color');
-        cell.addEventListener('click',turnClick, false);
-    });
+function startGame() {
+	message.textContent = "";
+	originalBoard = Array.from(Array(9).keys()); //create array with 9 elems, values from 0-8.
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].textContent = "";
+        cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+    }
 }
 
-function turnClick(clickEvent){
-    if(typeof originalBoard[clickEvent.target.id] === 'number'){ // if board content of cell is a number (cell is blank)
-        turn(clickEvent.target.id, humanPlayer); // wrapped in 'turnClick' because the 'turn' func can be called with human or AI player.
-        if(!checkTie() && !winnerFound){ //if not tied, com takes a turn, with square id gotten from bestMove()
+function turnClick(square) {
+	if (typeof originalBoard[square.target.id] == 'number') { // if board content of cell is a number (cell is blank)
+		turn(square.target.id, humanPlayer); // wrapped in 'turnClick' because the 'turn' func can be called with human or AI player.
+		if (!checkVictory(originalBoard, humanPlayer) && !checkTie()) { //if not tied and human didn't win, com takes a turn, with square id gotten from bestMove()
             turn(bestMove(), comPlayer);
         }
-    }
+	}
 }
 
-function turn(clickedCellID, player){
-    originalBoard[clickedCellID] = player;
-    document.getElementById(clickedCellID).textContent = player;
-    
-    let gameWon = checkVictory(originalBoard, player);
-    if(gameWon) {gameOver(gameWon);}
+function turn(squareId, player) {
+	originalBoard[squareId] = player;
+	document.getElementById(squareId).textContent = player;
+	let gameWon = checkVictory(originalBoard, player);
+	if (gameWon) {gameOver(gameWon);}
 }
 
-// board is 9 elems array [0,1,2,3,4,5,6,7,8] with 'X' or 'O' where players played.
-function checkVictory(board, player){
+function checkVictory(board, player) {
     //find places on board that has been played.
-    // let plays = board.reduce((accumulator, elem, index) => { //go through each elem of board
-    //     (elem === player) ? accumulator.concat(index) : accumulator; //if elem = player, add index to accumulator, if not, return accumulator without adding anything.
-    // },[]); //initialize accumulator to []
+    let plays = board.reduce((accumulator, elem, i) => //go through each elem of board
+        //if elem = player, add index to accumulator, if not, return accumulator without adding anything.
+		(elem === player) ? accumulator.concat(i) : accumulator, []); //initialized accumulator to []
+    
     let gameWon = null;
-
-    for(let i=0; i<winCombos.length; i++){
-        combo = winCombos[i];
-        if(board[combo[0]]===board[combo[1]]
-        && board[combo[1]]===board[combo[2]] //if match a winning combo,
-        && board[combo[0]] !== /^[\d]$/){ //and content is not a digit (should be 'O' or 'X').
-
-            winnerFound = true;
-            gameWon = {index: i, player: player};
-            break;
-        }
-    }
-    return gameWon; //null if no winner, hash if someone win (with which combo and which player won)
+	for (let [index, win] of winCombos.entries()) { // check if game has been won. winCombos.entries get both the index and element (a win combo)
+		if (win.every(elem => plays.indexOf(elem) > -1)) { //go through every elems in a win combo, check if plays.indexOf(elem) > -1). //It means check if player has played in every spots in a win combo.
+			gameWon = {index: index, player: player}; //if yes, get which player and which combo
+			break; //break out of for loop
+		}
+	}
+	return gameWon; //null or hash with which winner and which combo
 }
 
-function gameOver(gameWon){
+function gameOver(gameWon) {
     //highlight squares that are part of winning combo
     winCombos[gameWon.index].forEach(index => {
         document.getElementById(index).style.backgroundColor = 
             gameWon.player === humanPlayer ? "lightGreen" : "red"; //green if human, red if com
     });
 
-    //disable clicking on any squares
-    disableCells();
-
-    message.textContent = gameWon.player + " wins!";
-}
-
-function emptySquares(){
-    //get all elems with type 'number', which is empty
-    return originalBoard.filter(square => typeof square === 'number');
-}
-
-function bestMove(){
-    return emptySquares()[0]; //return first empty square
-}
-
-function checkTie(){
-    if(emptySquares().length === 0 && !winnerFound){ //if no empty squares and no winner found
-        disableCells();
-        message.textContent = "IT'S A DRAW";
-        return true;
+    //disable clicking on squares
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
     }
-    return false;
+    
+	declareWinner(gameWon.player == humanPlayer ? "YOU WIN !" : "YOU LOSE !");
 }
 
+function declareWinner(who) {
+	message.textContent = who;
+}
+
+function emptySquares() {
+	return originalBoard.filter(cell => typeof cell == 'number'); //get all elems with type 'number' from originalBoard, which means empty cells
+}
+
+function bestMove() {
+	return minimax(originalBoard, comPlayer).index; //return result of minimax .index because minimax return an object, .index is the index the AI will play
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) { // if no empty squares
+		for (let i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "lightBlue";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("TIE !")
+		return true;
+	}
+	return false;
+}
+
+function minimax(newBoard, player) {
+	let availCells = emptySquares();
+
+    //check for tereminal states
+	if (checkVictory(newBoard, humanPlayer)) { //human wins
+		return {score: -10}; //low score
+	} else if (checkVictory(newBoard, comPlayer)) { //COM wins
+		return {score: 10}; //high score
+	} else if (availCells.length === 0) { //draw
+		return {score: 0}; //0 score
+    }
+    
+    //collect scores from each empty squares to evaluate
+	let moves = [];
+	for (let i = 0; i < availCells.length; i++) {
+		let move = {};
+		move.index = newBoard[availCells[i]];
+		newBoard[availCells[i]] = player;
+
+		if (player === comPlayer) {
+			var result = minimax(newBoard, humanPlayer);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, comPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availCells[i]] = move.index; //reset newBoard to what it was before
+
+		moves.push(move); // push move object to moves arr
+	}
+
+	let bestMove; //evaluate bestMove in moves arr
+	if(player === comPlayer) { //choose move with highest score when AI is playing
+		let bestScore = -Infinity;
+		for(let i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else { //choose move with lowest score when human is playing
+		var bestScore = Infinity;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
+}
